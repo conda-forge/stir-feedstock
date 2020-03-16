9ad4c7ca5cdf9b7478c8da0a25de4d85908565b4
Kris Thielemans <k.thielemans@ucl.ac.uk>
2020-03-16 21:29:09 +0000

    Switch to Ninja generator on Windows
    Hopefully this gets around MSBuild problems.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,12 @@ cd build
 
 set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
+echo Start Windows build with
+echo CMAKE_GENERATOR "%CMAKE_GENERATOR%"
+echo LIBRARY_PREFIX %LIBRARY_PREFIX%
+echo PYTHON_LIBRARY %PYTHON_LIBRARY%
 :: Configure.
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -G "Ninja"
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D PYTHON_DEST=%SP_DIR% ^
       -D BUILD_SWIG_PYTHON:BOOL=ON ^
