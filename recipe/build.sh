3c95efd238de1d97c1837c8b02791fdb0bf40b9a
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2020-03-27 09:44:18 +0000

    set CMAKE_PREFIX_PATH to $PREFIX
    to increase likelihood that CMake finds conda stuff as opposed to system libraries etc

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 mkdir build && cd build
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_PREFIX_PATH=$PREFIX \
       -D PYTHON_DEST=$SP_DIR \
       -D BUILD_SWIG_PYTHON:BOOL=ON \
       -D CMAKE_BUILD_TYPE=Release \
