cb5cde7094b253e55cb17dd2ff98713e66a5755d
Christopher J. Wright <cjwright4242@gmail.com>
2020-08-10 15:57:47 -0400

    sorry for the bot closing the PR with your changes on it, I've done my best to restore it

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,18 @@
 mkdir build && cd build
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+debugbuild=true
+
+#if $debugbuild; then
+echo HIER
+    which python
+    find $PREFIX -name Python.h
+    find $PREFIX -name libpython\*
+
+    find /home/conda/feedstock_root/build_artifacts/  -name Python.h
+    find /home/conda/feedstock_root/build_artifacts/  -name libpy\*
+#fi
+exit 1
+cmake -D CMAKE_FIND_DEBUG_MODE=$debugbuild \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D PYTHON_DEST=$SP_DIR \
       -D BUILD_SWIG_PYTHON:BOOL=ON \
