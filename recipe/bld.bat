2989b87814211d935482c46870f7e9a49c8a235d
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-03-23 08:13:09 +0000

    Windows build script changes
    specify Python_EXECUTABLE is CMake was picking up a Python 2 version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,13 @@
 mkdir build
 cd build
 
-set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
-
 echo Start Windows build
 :: Configure.
 cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D PYTHON_DEST=%SP_DIR% ^
       -D BUILD_SWIG_PYTHON:BOOL=ON ^
-      -D PYTHON_LIBRARY=%PYTHON_LIBRARY% ^
+      -D Python_EXECUTABLE=%PYTHON% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D STIR_OPENMP=ON ^
       -D GRAPHICS=None ^
