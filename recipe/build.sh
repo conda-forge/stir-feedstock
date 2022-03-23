6f912784678e2e89f175e1e4395ba5109d002721
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-03-23 08:12:25 +0000

    Linux build script changes
    - use Ninja (make is no longer installed)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
 mkdir build && cd build
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -G "Ninja" \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D PYTHON_DEST=$SP_DIR \
       -D BUILD_SWIG_PYTHON:BOOL=ON \
+      -D Python_EXECUTABLE=${which python} \
       -D CMAKE_BUILD_TYPE=Release \
       -D STIR_OPENMP=ON \
       -D GRAPHICS=None \
       $SRC_DIR
-make -j${CPU_COUNT}
-make install
+
+# Build.
+cmake --build . --config Release
+
+# Install
+cmake --build . --target install --config Release
