ff565859ced020de01a686b4ef2e197d3e55c3ce
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-04-19 14:00:21 +0100

    correct location of config dir on Windows

--- recipe/activate.ps1
+++ recipe/activate.ps1
@@ -1,5 +1,5 @@
 # set where STIR installed JSON files are
-$Env:STIR_CONFIG_DIR="$Env:CONDA_PREFIX\share\stir\config"
+$Env:STIR_CONFIG_DIR="$Env:CONDA_PREFIX\Library\share\stir\config"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $Env:STIR_CONFIG_DIR"
 
