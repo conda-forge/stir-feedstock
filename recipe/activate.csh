ac8f3e2558115a9ece4c4bb56fe5151690723de9
Kris Thielemans <k.thielemans@ucl.ac.uk>
2024-05-24 14:52:03 +0100

    update to 6.1.0
    also follow  https://conda-forge.org/news/2024/03/24/stdlib-migration/
    also remove olivierverdier as maintainer

--- recipe/activate.csh
+++ recipe/activate.csh
@@ -1,5 +1,5 @@
 # set where STIR installed JSON files are
-setenv STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.0/config"
-setenv STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.0"
+setenv STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.1/config"
+setenv STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.1"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $STIR_CONFIG_DIR"
--- recipe/activate.fish
+++ recipe/activate.fish
@@ -1,5 +1,5 @@
 # set where STIR installed JSON files are
-set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.0/config"
-set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.0"
+set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.1/config"
+set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.1"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $STIR_CONFIG_DIR"
