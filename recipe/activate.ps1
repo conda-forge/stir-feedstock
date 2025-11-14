f36eae032021c94308b3ad1698efba788c4b1841
Kris Thielemans <k.thielemans@ucl.ac.uk>
2025-11-14 08:20:09 +0000

    set to master 14Nov2024 and update activation scripts to 6.4.0

--- recipe/activate.ps1
+++ recipe/activate.ps1
@@ -1,6 +1,6 @@
 # set where STIR installed JSON files are
-$Env:STIR_CONFIG_DIR="$Env:CONDA_PREFIX\Library\share\STIR-6.3\config"
-$Env:STIR_DOC_DIR="$Env:CONDA_PREFIX\Library\share\doc\STIR-6.3"
+$Env:STIR_CONFIG_DIR="$Env:CONDA_PREFIX\Library\share\STIR-6.4\config"
+$Env:STIR_DOC_DIR="$Env:CONDA_PREFIX\Library\share\doc\STIR-6.4"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $Env:STIR_CONFIG_DIR"
 
