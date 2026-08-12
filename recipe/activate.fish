008ef385eaaefd806798725c487b1de8983fe63a
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-08-12 12:27:02 +0100

    update to 6.5.0

--- recipe/activate.fish
+++ recipe/activate.fish
@@ -1,5 +1,5 @@
 # set where STIR installed JSON files are
-set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.4/config"
-set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.4"
+set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.5/config"
+set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.5"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $STIR_CONFIG_DIR"
