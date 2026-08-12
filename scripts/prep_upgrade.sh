a76837671441b9dafa8e47c1b8e09aed0a1f325d
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-08-12 12:18:17 +0100

    update scripts and recipe for easier maintenance

--- scripts/prep_upgrade.sh
+++ scripts/prep_upgrade.sh
@@ -1,4 +1,12 @@
-#! /bin/sh
+#! /usr/bin/env bash
+# Usage:
+# cd recipe
+# ../scripts/prep_upgrade.sh 7.2.1
 ver=$1
-for f in activate*; do sed -i "s#STIR-6\..#STIR-${ver}#" $f;done
-sed -i -e "s#6\..#${ver}#" -e 's#{% set build_number = . %}#{% set build_number = 0 %}#' meta.yaml
+majorminor_ver=${ver%\.[0-9]*}
+for f in activate*; do sed -i -e "s#STIR-[0-9]*\.[0-9]*#STIR-${majorminor_ver}#" $f;done
+sed -i \
+    -e "s# version: *\".*# version: \"${ver}\"#g" \
+    -e 's#build_number:.*#build_number: 0#g' \
+    -e 's#dev_string:.*#dev_string: "dev0"#g' \
+    recipe.yaml
