a654039a92ee2dc433d41693449416c0b42d6208
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-09-24 20:30:07 +0100

    update to STIR version 6.4.0
    also update the maintainer script to make this easier

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,6 +1,6 @@
 @REM set where STIR installed JSON files are
-@set "STIR_CONFIG_DIR=%CONDA_PREFIX%\Library\share\STIR-6.3\config"
-@set "STIR_DOC_DIR=%CONDA_PREFIX%\Library\share\doc\STIR-6.3"
+@set "STIR_CONFIG_DIR=%CONDA_PREFIX%\Library\share\STIR-6.4\config"
+@set "STIR_DOC_DIR=%CONDA_PREFIX%\Library\share\doc\STIR-6.4"
 
 @REM echo ACTIVATED STIR_CONFIG_DIR TO %STIR_CONFIG_DIR%
 
--- recipe/activate.fish
+++ recipe/activate.fish
@@ -1,5 +1,5 @@
 # set where STIR installed JSON files are
-set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.3/config"
-set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.3"
+set -gx STIR_CONFIG_DIR "${CONDA_PREFIX}/share/STIR-6.4/config"
+set -gx STIR_DOC_DIR "${CONDA_PREFIX}/share/doc/STIR-6.4"
 
 # echo "STIR ACTIVATE STIR_CONFIG_DIR TO $STIR_CONFIG_DIR"
